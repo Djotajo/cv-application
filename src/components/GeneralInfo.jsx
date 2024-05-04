bc3f3fa018fb45313a73a0498408dc4f717a5e84
Djotajo <20823564+Djotajo@users.noreply.github.com>
2024-05-04 21:17:21 +0200

    Prepare raw GeneralInfo component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,24 +1,81 @@
+import { useState } from "react";
+
 function GeneralInfo() {
-  return (
-    <>
-      <form action="#">
-        <h2>General Info</h2>
-        <label htmlFor="firstName">
-          First Name <input type="text" id="firstName" name="firstName" />
-        </label>
-        <label htmlFor="lastName">
-          Last Name <input type="text" id="lastName" name="lastName" />
-        </label>
-        <label htmlFor="email">
-          E-mail <input type="email" id="email" name="email" />
-        </label>
-        <label htmlFor="phone">
-          Phone <input type="text" id="phone" name="phone" />
-        </label>
-        <button>Submit</button>
-      </form>
-    </>
-  );
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  function handleSubmit() {
+    setIsSubmitted(true);
+  }
+
+  function handleEdit() {
+    setIsSubmitted(false);
+  }
+
+  if (!isSubmitted) {
+    return (
+      <>
+        <form action="#">
+          <h2>General Info</h2>
+          <label htmlFor="firstName">
+            First Name{" "}
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </label>
+          <label htmlFor="lastName">
+            Last Name{" "}
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </label>
+          <label htmlFor="email">
+            E-mail{" "}
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor="phone">
+            Phone{" "}
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </label>
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2>Success</h2>
+        <p>First name - {firstName}</p>
+        <p>Last name - {lastName}</p>
+        <p>Email - {email}</p>
+        <p>Phone - {phone}</p>
+        <button onClick={handleEdit}>Edit</button>
+      </>
+    );
+  }
 }
 
 export default GeneralInfo;
